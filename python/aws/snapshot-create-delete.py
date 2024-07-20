399f3b919a016beacdc5d0c1f3fdf8dcf61d88b5
DavidM <mund3rsinthecloud@proton.me>
2024-07-20 21:14:34 +0100

    sanitsed scripts

--- python/aws/snapshot-create-delete.py
+++ python/aws/snapshot-create-delete.py
@@ -16,7 +16,7 @@ def delete_rds_snapshot(snapshot_id):
     print(f"Deleting snapshot {snapshot_id}")
 
 if __name__ == "__main__":
-    db_instance_id = 'your-db-instance-id'
-    snapshot_id = 'your-snapshot-id'
+    db_instance_id = 'db-instance-id'
+    snapshot_id = 'snapshot-id'
     create_rds_snapshot(db_instance_id, snapshot_id)
     # delete_rds_snapshot(snapshot_id)  # Uncomment to delete the snapshot
